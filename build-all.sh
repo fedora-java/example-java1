eb9a2e80e9ad7384b2764cbb0d52729a0593476d
Michael Simacek <msimacek@redhat.com>
2017-05-30 13:43:26 +0200

    Put all build products in single directory

--- build-all.sh
+++ build-all.sh
@@ -8,11 +8,11 @@ fi
 
 packages="example-java1 maven-foo maven-bar ivy-baz ivy-publish"
 
-rm -rf srpms/ meta-repo/ collection-repo/ result/
-mkdir srpms meta-repo collection-repo
+rm -rf work
+mkdir -p work/{srpms,repodata}
 for pkg in $packages; do
-    rpmbuild -bs -D"_sourcedir $pkg" -D"_srcrpmdir srpms" $pkg/*.spec
-    [ $pkg == example-java1 ] || srpm_list="$srpm_list `echo srpms/$pkg-*.src.rpm`"
+    rpmbuild -bs -D"_sourcedir $pkg" -D"_srcrpmdir work/srpms" $pkg/*.spec
+    [ $pkg == example-java1 ] || srpm_list="$srpm_list `echo work/srpms/$pkg-*.src.rpm`"
 done
 
 python << EOF
@@ -22,18 +22,18 @@ with open("$1") as f:
 exec(code)
 config_opts['chroot_setup_cmd'] = "install @build scl-utils-build $2-scldevel"
 config_opts['root'] = 'example-java1-meta'
-with open("tmp-config-meta.cfg", 'w') as br_dest:
+with open("work/tmp-config-meta.cfg", 'w') as br_dest:
     for k, v in list(config_opts.items()):
         br_dest.write("config_opts[%r] = %r\n" % (k, v))
-config_opts['yum.conf'] += "\n[meta]\nbaseurl=file://$PWD/meta-repo"
 config_opts['chroot_setup_cmd'] = "install @build example-java1-build"
 config_opts['root'] = 'example-java1-collection'
-with open("tmp-config-collection.cfg", 'w') as br_dest:
+with open("work/tmp-config-collection.cfg", 'w') as br_dest:
     for k, v in list(config_opts.items()):
         br_dest.write("config_opts[%r] = %r\n" % (k, v))
 EOF
 
-mock -r tmp-config-meta.cfg srpms/example-java1-*.src.rpm --resultdir=result
-mv result/example-java1-*.rpm meta-repo/
-(cd meta-repo && createrepo_c .)
-mockchain -r tmp-config-collection.cfg ${srpm_list}
+mock -r work/tmp-config-meta.cfg work/srpms/example-java1-*.src.rpm --resultdir=work/
+mkdir -p work/results/tmp-config-collection/
+mv work/example-java1-*.rpm work/results/tmp-config-collection/
+(cd work/results/tmp-config-collection && createrepo_c .)
+mockchain -r work/tmp-config-collection.cfg -l work ${srpm_list}
